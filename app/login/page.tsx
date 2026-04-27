c25302ca1ad1717e6546ecaf9a91f6b483831be8
Arlon Moraes Lima <arlon.moraes@bba.ind.br>
2026-04-27 16:11:42 -0300

    login users

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,65 @@
-export default function Page() {
+'use client'
+
+import { useState } from 'react'
+import { supabase } from '@/app/lib/supabase'
+import { useRouter } from 'next/navigation'
+
+export default function Login() {
+  const router = useRouter()
+
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+  const [carregando, setCarregando] = useState(false)
+
+  async function entrar() {
+    if (!email || !senha) {
+      alert('Informe e-mail e senha')
+      return
+    }
+
+    setCarregando(true)
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password: senha,
+    })
+
+    setCarregando(false)
+
+    if (error) {
+      alert('E-mail ou senha inválidos')
+      console.log(error)
+      return
+    }
+
+    router.push('/eventos')
+  }
+
   return (
     <div style={{ padding: 20 }}>
-      <h1>Página funcionando</h1>
+      <h1>Login</h1>
+
+      <input
+        type="email"
+        placeholder="E-mail"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="Senha"
+        value={senha}
+        onChange={(e) => setSenha(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={entrar} disabled={carregando}>
+        {carregando ? 'Entrando...' : 'Entrar'}
+      </button>
     </div>
-  );
+  )
 }
