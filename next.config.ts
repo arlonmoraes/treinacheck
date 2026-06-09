510900beb43c9b8af4ddc05f258bc3a7d36ef850
Arlon Moraes Lima <arlon.moraes@bba.ind.br>
2026-06-09 14:45:22 -0300

    usuario app PWA

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next'
+import withPWAInit from '@ducanh2912/next-pwa'
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development', // Mantém o PWA desligado enquanto você programa no PC
+})
 
 const nextConfig: NextConfig = {
   devIndicators: false,
 }
 
-export default nextConfig
+// Exporta a sua configuração original, mas agora "envelopada" pelo motor do PWA
+export default withPWA(nextConfig)
